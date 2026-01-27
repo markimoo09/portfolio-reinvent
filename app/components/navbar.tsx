8c25ae403ac86f7ed831c68cf26edbcb3a02f657
Mark Christopher Lumbay <lumbaymark@gmail.com>
2026-01-27 19:03:16 +0800

    increased navbar and grid-lines clearance and improved typography of nav

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -5,11 +5,11 @@ const Navbar = () => {
   return (
     <>
       <div className="sticky top-0 z-50 w-full bg-gruv-bg">
-        <header className="py-6 mx-auto max-w-6xl px-4 sm:px-6 lg:px-8 items-center">
+        <header className="py-10 mx-auto max-w-6xl px-4 sm:px-6 lg:px-8 items-center">
           <div className="w-full flex flex-row justify-between items-center">
             <div>
               <h2>
-                <Link href="/" className="text-3xl font-bold">
+                <Link href="/" className="text-4xl font-bold">
                   Markimoo
                   <span className="text-gruv-highlight-green">.dev</span>
                 </Link>
@@ -22,7 +22,7 @@ const Navbar = () => {
                   <li>
                     <Link
                       href="/about"
-                      className="text-lg font-semibold hover:text-gruv-highlight-yellow text-gruv-highlight-yellow transition-colors duration-300"
+                      className="text-xl font-semibold hover:text-gruv-highlight-yellow text-gruv-highlight-yellow transition-colors duration-300"
                     >
                       Who am I?
                     </Link>
@@ -30,7 +30,7 @@ const Navbar = () => {
                   <li>
                     <Link
                       href="/projects"
-                      className="text-lg font-semibold hover:text-gruv-highlight-yellow text-gray-300 transition-colors duration-200"
+                      className="text-xl font-semibold hover:text-gruv-highlight-yellow text-gray-300 transition-colors duration-200"
                     >
                       Experience
                     </Link>
@@ -38,15 +38,15 @@ const Navbar = () => {
                   <li>
                     <Link
                       href="/projects"
-                      className="text-lg font-semibold hover:text-gruv-highlight-yellow text-gray-300 transition-colors duration-200"
+                      className="text-xl font-semibold hover:text-gruv-highlight-yellow text-gray-300 transition-colors duration-200"
                     >
-                      What I've Built
+                      Projects
                     </Link>
                   </li>
                   <li>
                     <Link
                       href="/contact"
-                      className="text-lg font-semibold hover:text-gruv-highlight-yellow text-gray-300 transition-colors duration-200"
+                      className="text-xl font-semibold hover:text-gruv-highlight-yellow text-gray-300 transition-colors duration-200"
                     >
                       Contact Me
                     </Link>

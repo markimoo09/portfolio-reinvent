d9f6d666395e8cc34d38b57a5c153919ceb62650
Mark Christopher Lumbay <lumbaymark@gmail.com>
2026-01-27 18:58:47 +0800

    created grid-lines bg and adjusted navbar to block entier width

---
+++ app/components/about_me.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const AboutMe = () => {
+  return (
+    <>
+      <div className="h-screen">
+      <div className="absolute inset-0 h-full w-full bg-gruv-bg bg-[linear-gradient(to_right,#8080800c_1px,transparent_1px),linear-gradient(to_bottom,#8080800c_1px,transparent_1px)] bg-size-[120px_120px] [mask-image:radial-gradient(80%_70%_at_50%_50%,black_55%,transparent_100%),linear-gradient(to_right,transparent_0%,black_18%,black_82%,transparent_100%),linear-gradient(to_bottom,transparent_0%,black_18%,black_82%,transparent_100%)] [mask-composite:intersect] [mask-repeat:no-repeat] [mask-size:100%_100%,100%_100%,100%_100%]"></div>
+      </div>
+    </>
+  );
+};
+
+export default AboutMe;
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
 import React from "react";
+import AboutMe from "./components/about_me";
 
 const HomePage = () => {
   return (
-    <div className="py-6 h-screen mx-auto max-w-6xl px-4 sm:px-6 lg:px-8"></div>
+    <div className="py-6 h-screen mx-auto max-w-6xl px-4 sm:px-6 lg:px-8">
+      <AboutMe />
+    </div>
   );
 };
 

689f048ae4d620985d3e9d403b9c53ffb229b105
Mark Christopher Lumbay <lumbaymark@gmail.com>
2026-01-27 18:14:28 +0800

    set main container dimensions and spacing

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 const HomePage = () => {
-  return <div className="p-6">HomePage</div>;
+  return (
+    <div className="py-6 h-screen mx-auto max-w-6xl px-4 sm:px-6 lg:px-8"></div>
+  );
 };
 
 export default HomePage;

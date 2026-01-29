09bbab3956e398e5f52ffd29982542e7528200eb
Mark Christopher Lumbay <lumbaymark@gmail.com>
2026-01-29 17:41:08 +0800

    fixed scrolling anim

--- app/components/about_me.tsx
+++ app/components/about_me.tsx
@@ -10,7 +10,7 @@ const AboutMe = () => {
   return (
     <>
       <div className="relative w-full min-h-[calc(100vh-7.5rem)] flex items-center">
-        <div className="fixed inset-0 -z-10 bg-gruv-bg bg-[linear-gradient(to_right,#8080801c_1px,transparent_1px),linear-gradient(to_bottom,#8080801c_1px,transparent_1px)] bg-size-[120px_120px] mask-[radial-gradient(circle_at_50%_50%,black_0%,black_18%,transparent_42%),linear-gradient(to_right,transparent_0%,black_46%,black_54%,transparent_100%),linear-gradient(to_bottom,transparent_0%,black_46%,black_54%,transparent_100%)] mask-composite-intersect mask-repeat-no-repeat [mask-size:100%_100%,100%_100%,100%_100%] pointer-events-none"></div>
+        <div className="fixed inset-0 -z-10 bg-gruv-bg bg-[linear-gradient(to_right,#8080801c_1px,transparent_1px),linear-gradient(to_bottom,#8080801c_1px,transparent_1px)] bg-size-[120px_120px] mask-[radial-gradient(circle_at_50%_50%,black_0%,black_18%,transparent_42%),linear-gradient(to_right,transparent_0%,black_46%,black_54%,transparent_100%),linear-gradient(to_bottom,transparent_0%,black_46%,black_54%,transparent_100%)] mask-composite-intersect mask-repeat-no-repeat [mask-100%_100%,100%_100%,100%_100%] pointer-events-none"></div>
         <div className="flex w-full flex-col gap-18 py-6">
           <div className="relative z-10 flex flex-col gap-10 lg:flex-row lg:justify-between lg:items-start">
             <div className="flex flex-1 min-w-0 flex-col gap-6">

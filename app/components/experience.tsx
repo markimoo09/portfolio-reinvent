8491a57d5801f736e975e4d977d8098fb4c853c6
Mark Christopher Lumbay <lumbaymark@gmail.com>
2026-01-29 19:30:04 +0800

    scaffolded layout and containers for exp section

---
+++ app/components/experience.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const ExperienceSection = () => {
+  return (
+    <div className="h-screen w-full grid md:grid-cols-5 grid-cols-1 gap-8 py-6">
+      <div className="outline-1 outline-gruv-highlight-green/40 bg-gruv-bg-tinted row-span-2 col-span-3 rounded-xl"></div>
+      <div className="outline-1 outline-gruv-highlight-green/40 bg-gruv-bg-tinted row-span-1 col-span-2 rounded-xl"></div>
+      <div className="outline-1 outline-gruv-highlight-green/40 bg-gruv-bg-tinted row-span-1 col-span-2 rounded-xl"></div>
+      <div className="outline-1 outline-gruv-highlight-green/40 bg-gruv-bg-tinted row-span-1 col-span-2 rounded-xl"></div>
+      <div className="outline-1 outline-gruv-highlight-green/40 bg-gruv-bg-tinted row-span-2 col-span-3 rounded-xl"></div>
+      <div className="outline-1 outline-gruv-highlight-green/40 bg-gruv-bg-tinted row-span-1 col-span-2 rounded-xl"></div>
+    </div>
+  );
+};
+
+export default ExperienceSection;
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import AboutMe from "./components/about_me";
+import ExperienceSection from "./components/experience";
 
 const HomePage = () => {
   return (
     <div className="mx-auto max-w-6xl px-4 sm:px-6 lg:px-8">
       <AboutMe />
+      <ExperienceSection />
     </div>
   );
 };

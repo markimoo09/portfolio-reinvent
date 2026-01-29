f5accaaacc2bf728ee4fbb281779ccce2f7594c6
Mark Christopher Lumbay <lumbaymark@gmail.com>
2026-01-29 22:44:19 +0800

    improved spacing of main sections

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,10 @@ import ExperienceSection from "./components/experience";
 const HomePage = () => {
   return (
     <div className="mx-auto max-w-6xl px-4 sm:px-6 lg:px-8">
-      <AboutMe />
-      <ExperienceSection />
+      <div className="flex flex-col gap-14 ">
+        <AboutMe />
+        <ExperienceSection />
+      </div>
     </div>
   );
 };

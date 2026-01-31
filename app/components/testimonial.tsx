7edaeb70d6597353568783159458bc34aaa35f2d
Mark Christopher Lumbay <lumbaymark@gmail.com>
2026-01-31 23:37:02 +0800

    added profile pic to the testimonials

--- app/components/testimonial.tsx
+++ app/components/testimonial.tsx
@@ -5,6 +5,7 @@ import React from "react";
 type Testimonial = {
   testimony: string;
   author: string;
+  position: string;
   key: string;
 };
 
@@ -12,48 +13,56 @@ const testimonials: Testimonial[] = [
   {
     testimony: "Hardworking, You'll be sure to get quality outputs from him.",
     author: "John Doe",
+    position: "Software Engineer",
     key: crypto.randomUUID(),
   },
   {
     testimony:
       "Mark Christopher demonstrates a well-rounded level of professional excellence through strong technical proficiency and adaptability. He consistently delivers high-quality results, even in challenging or high-pressure environments.",
     author: "John Doe",
+    position: "Software Engineer",
     key: crypto.randomUUID(),
   },
   {
     testimony:
       "Great team member and can effectively communicate and get along with other members to make sure goals are met.",
     author: "John Doe",
+    position: "Software Engineer",
     key: crypto.randomUUID(),
   },
   {
     testimony:
       "Great team member and can effectively communicate and get along with other members to make sure goals are met.",
     author: "John Doe",
+    position: "Software Engineer",
     key: crypto.randomUUID(),
   },
   {
     testimony:
       "Great team member and can effectively communicate and get along with other members to make sure goals are met.",
     author: "John Doe",
+    position: "Software Engineer",
     key: crypto.randomUUID(),
   },
   {
     testimony:
       "Great team member and can effectively communicate and get along with other members to make sure goals are met.",
     author: "John Doe",
+    position: "Software Engineer",
     key: crypto.randomUUID(),
   },
   {
     testimony:
       "Great team member and can effectively communicate and get along with other members to make sure goals are met.",
     author: "John Doe",
+    position: "Software Engineer",
     key: crypto.randomUUID(),
   },
   {
     testimony:
       "Great team member and can effectively communicate and get along with other members to make sure goals are met.",
     author: "John Doe",
+    position: "Software Engineer",
     key: crypto.randomUUID(),
   },
 ];
@@ -68,7 +77,7 @@ const TestimonialSection = () => {
 
       <div className="w-full overflow-hidden px-4">
         <div
-          className="animate-testimonial-marquee flex w-max gap-4"
+          className="animate-testimonial-marquee flex w-max gap-4 items-start"
           onMouseEnter={(e) =>
             e.currentTarget.classList.add("animate-testimonial-marquee-paused")
           }
@@ -82,15 +91,26 @@ const TestimonialSection = () => {
             return (
               <div
                 key={`${testimony.key}-${i}`}
-                className="transition-color duration-200 border border-transparent hover:border-gruv-highlight-yellow flex-none w-96 md:w-96 snap-start flex flex-col p-6 min-h-40 justify-between gap-12 bg-gruv-bg-tinted rounded-xl"
+                className="transition-color duration-200 border border-transparent hover:border-gruv-highlight-yellow flex-none w-96 md:w-96 snap-start flex flex-col p-6 min-h-40 gap-3 bg-gruv-bg-tinted rounded-xl"
               >
+                <div className="flex gap-3 items-stretch">
+                  <div className="rounded-full bg-gruv-highlight-yellow text-white w-8 h-8 items-center justify-center flex ">
+                    {testimony.author[0]}
+                  </div>
+
+                  <div className="flex flex-col">
+                    <h3 className="text-lg font-bold text-white ">
+                      {testimony.author}
+                    </h3>
+                    <p className="text-sm text-gray-400">
+                      {testimony.position}
+                    </p>
+                  </div>
+                </div>
+
                 <p className="text-md font-stretch-extra-condensed">
                   {testimony.testimony}
                 </p>
-
-                <h3 className="text-lg font-bold text-gruv-highlight-yellow text-right">
-                  {testimony.author}
-                </h3>
               </div>
             );
           })}
